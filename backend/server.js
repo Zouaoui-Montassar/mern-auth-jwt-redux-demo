27d86dd68ddd7d78cbea4fe9f035d87d24700069
Zouaoui-Montassar <zouaouimonta12345@gmail.com>
2024-09-03 22:17:00 +0100

    custom error middleware

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import userRoutes from './routes/userRoutes.js'
 dotenv.config();
+import { notFound,errorHandler } from './middleware/errorMiddleware.js';
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use('/api/users',userRoutes);
+
 app.get('/',(req,res) => 
     res.send('server is ready')
 )
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => 
     console.log(`server started on port ${PORT}`)
